017764744e7e35bd8b06fcd7337a14750cdb0f2e
Yuriy Tymkiv <11hacha11@gmail.com>
2018-04-17 01:47:47 +0300

    deleted index and style, 1 array for nodes, 1 function getParameters, s and ms, add rotate

--- js/task.js
+++ js/task.js
@@ -1,14 +1,24 @@
-let Nodes = getElementsByAttribute('animation');
-let animationNodes = Nodes.map(function(item) {
-    return {
-        element: item,
-        offsetTop: getOffsetTop(item),
-        height: getHeight(item),
-        type: item.getAttribute('animation'),
-        duration: item.getAttribute('duration'),
-        status: false
+let animationNodes = getElementsByAttribute('animation');
+
+function getElementsByAttribute(attr) {
+    let matchElements = [];
+    let allElements = document.getElementsByTagName('*');
+    for (let i=0; i<allElements.length; i++) {
+        if (allElements[i].getAttribute(attr) !== null) {
+            matchElements.push(allElements[i]);
+        };
     };
-});
+    matchElements = matchElements.map(function(item, i) {
+        return {
+            element: item,
+            param: getParameters(item),
+            type: item.getAttribute('animation'),
+            duration: item.getAttribute('duration'),
+            status: false
+        }
+    }, 0);
+    return matchElements;
+}
 
 function stats() {
     console.log(animationNodes);
@@ -20,9 +30,9 @@ window.onscroll = checking;
 function checking() {
     let scrolled = window.pageYOffset || document.documentElement.scrollTop;
     animationNodes.reduce(function(prev, curr, i) {
-        if (animationNodes[i].offsetTop < scrolled+document.documentElement.clientHeight && animationNodes[i].offsetTop+animationNodes[i].height > scrolled) {
+        if (animationNodes[i].param.offsetTop < scrolled+document.documentElement.clientHeight && animationNodes[i].param.offsetTop+animationNodes[i].param.height > scrolled) {
             if (!animationNodes[i].status) {
-                animate(animationNodes[i].element, animationNodes[i].type, animationNodes[i].duration);
+                animate(animationNodes[i].element, animationNodes[i].type, animationNodes[i].duration, animationNodes[i].param);
             }
             animationNodes[i].status = true;
         }
@@ -32,9 +42,36 @@ function checking() {
     }, 0);
 }
 
-function animate(element, type, duration) {
+function getParameters(elem) {
+    let height = elem.clientHeight || elem.offsetHeight;
+    let width = elem.clientWidth || elem.offsetWidth;
+    let box = elem.getBoundingClientRect();
+    let body = document.body;
+    let docElem = document.documentElement;
+    let scrollTop = window.pageYOffset || docElem.scrollTop || body.scrollTop;
+    let clientTop = docElem.clientTop || body.clientTop || 0;
+    let top  = box.top +  scrollTop - clientTop;
+    let scrollLeft = window.pageXOffset || docElem.scrollLeft || body.scrollLeft;
+    let clientLeft = docElem.clientLeft || body.clientLeft || 0;
+    let left = box.left +  scrollLeft - clientLeft;
+    return {
+        height: Math.round(height),
+        width: Math.round(width),
+        offsetTop: Math.round(top),
+        offsetLeft: Math.round(left)
+    };
+}
+
+function animate(element, type, duration, param) {
     if (duration === null) {
         duration = '0.5s';
+    }
+    let durms = '';
+    if (typeof(duration) !== 'number') {
+        for (let i=0; i<duration.length-1; i++) {
+            durms+=duration[i];
+        }
+        durms*=1000;
     }
     switch(type) {
         case 'opacity':
@@ -44,80 +81,52 @@ function animate(element, type, duration) {
                 element.style.opacity = 1;
                 setTimeout(function() {
                     element.style.transition = '';
-                }, 500);
+                }, durms);
             }, 50);
             break;
         case 'shake':
-            element.style.transition = 'left 0.05s';
+            element.style.transition = 'transform 0.05s';
             let start = Date.now();
             let shaked = false;
             let timer = setInterval(function() {
                 let timePassed = Date.now() - start;
-                if (timePassed >= 500) {
+                if (timePassed >= durms) {
                     clearInterval(timer);
+                    element.style.transform = 'translateX(' + 0 + 'px)';
                     return;
                 }
                 if (!shaked) {
                     shaked = true;
-                    element.style.left = '100px';
+                    element.style.transform = 'translateX(' + -20 + 'px)';
                 } else if (shaked) {
                     shaked = false;
-                    element.style.left = '10px';
+                    element.style.transform = 'translateX(' + 20 + 'px)';
                 }
             }, 50);
             break;
         case 'leftin':
             element.style.opacity = 0;
-            let startpos = -getWidth(element);
-            let startoff = getOffsetLeft(element);
-            element.style.transform = 'translateX(' + (startpos-startoff) + 'px)';
-            let offset = -(startpos-startoff);
+            element.style.transform = 'translateX(' + (-(param.width)-param.offsetLeft) + 'px)';
             setTimeout(function() {
                 element.style.transition = 'transform ' + duration + ',' + 'opacity ' + duration;
                 element.style.transform  = 'translateX(' + 0 + 'px)';
                 element.style.opacity = 1;
                 setTimeout(function() {
                     element.style.transition = '';
-                }, 500);
+                }, durms);
             }, 50);
+            break;
+        case 'rotate':
+            element.style.opacity = 0;
+            setTimeout(function() {
+                element.style.transition = 'transform ' + duration + ',' + 'opacity ' + duration;
+                element.style.transform  = 'rotate(' + 360 + 'deg)';
+                element.style.opacity = 1;
+                setTimeout(function() {
+                    element.style.transition = '';
+                    element.style.transform  = 'rotate(' + 0 + 'deg)';
+                }, durms);
+            }, 50);
+            break;
     }
-}
-
-function getElementsByAttribute(attr) {
-    let matchElements = [];
-    let allElements = document.getElementsByTagName('*');
-    for (let i=0; i<allElements.length; i++) {
-        if (allElements[i].getAttribute(attr) !== null) {
-            matchElements.push(allElements[i]);
-        }
-    }
-    return matchElements;
-}
-
-function getHeight(elem) {
-    return elem.clientHeight || elem.offsetHeight;
-}
-
-function getWidth(elem) {
-    return elem.clientWidth || elem.offsetWidth;
-}
-
-function getOffsetTop(elem) {
-    let box = elem.getBoundingClientRect();
-    let body = document.body;
-    let docElem = document.documentElement;
-    var scrollTop = window.pageYOffset || docElem.scrollTop || body.scrollTop;
-    var clientTop = docElem.clientTop || body.clientTop || 0;
-    var top  = box.top +  scrollTop - clientTop;
-    return Math.round(top);
-}
-
-function getOffsetLeft(elem) {
-    let box = elem.getBoundingClientRect();
-    let body = document.body;
-    let docElem = document.documentElement;
-    var scrollLeft = window.pageXOffset || docElem.scrollLeft || body.scrollLeft;
-    var clientLeft = docElem.clientLeft || body.clientLeft || 0;
-    var left = box.left +  scrollLeft - clientLeft;
-    return Math.round(left);
 }
